2786425c32197d6916842d796e1377aecbc7e984
xutao <xutao@gmail.com>
2026-01-14 11:30:46 +0800

    init

--- assets/scripts/common/Global.ts
+++ assets/scripts/common/Global.ts
@@ -1,7 +1,7 @@
 import { _decorator, Component, director } from 'cc';
 import {Net} from "db://assets/scripts/common/Net";
+import {WirePayload} from "db://assets/scripts/wire/base/message";
 const { ccclass } = _decorator;
-
 
 @ccclass('Global')
 export class Global extends Component {
@@ -23,6 +23,10 @@ export class Global extends Component {
 
     protected onDestroy(): void {
         this.net.close();
+    }
+
+    static sendRequest<T extends WirePayload>(req: T) {
+        this.inst.net.sendRequest(req);
     }
 }
 
--- assets/scripts/hall_scene/HallMrg.ts
+++ assets/scripts/hall_scene/HallMrg.ts
@@ -1,21 +1,31 @@
 import { _decorator, Component } from 'cc';
+import {AtlasWireMessage} from "db://assets/scripts/wire/base/message";
+import {GetTableResp} from "db://assets/scripts/wire/payload/GetTableResp";
+import {eventBus} from "db://assets/scripts/common/EventBus";
+import {GetTableReq} from "db://assets/scripts/wire/payload/GetTableReq";
+import {Global} from "db://assets/scripts/common/Global";
 const { ccclass } = _decorator;
 
 @ccclass('HallMrg')
 export class HallMrg extends Component {
 
+    private getTableHandler: ((msg: AtlasWireMessage<GetTableResp>) => void) | null = null;
+
+    onGetTableClick(){
+        let getTableReq = new GetTableReq({});
+        Global.sendRequest(getTableReq);
+    }
+
     onEnable(){
-
+        this.getTableHandler = (msg: AtlasWireMessage<GetTableResp>) => {
+            console.log("table list",msg.payload.tables)
+        }
+        eventBus.on(GetTableResp.METHOD,this.getTableHandler)
     }
 
-    start() {
-        console.log('start')
 
-    }
 
-    update(_deltaTime: number) {
-        //console.log('deltaTime')
-    }
+
 }
 
 
