0653cae56a465c574383c5277b7beb6b09847ec5
xutao <xutao@gmail.com>
2026-01-16 15:39:44 +0800

    init

--- assets/scripts/common/Global.ts
+++ assets/scripts/common/Global.ts
@@ -3,6 +3,8 @@ import {Net} from "db://assets/scripts/common/Net";
 import {WirePayload} from "db://assets/scripts/wire/base/Message";
 const { ccclass } = _decorator;
 
+//  27374D  526D82  9DB2BF  DDE6ED
+
 @ccclass('Global')
 export class Global extends Component {
     static inst: Global;
